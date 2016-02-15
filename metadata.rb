99a594d6a55a4a6eb86ccab814d16a7120bf410f
Chris Blackburn <christopher.blackburn@gmail.com>
2016-02-15 10:17:33 +0000

    Add missing name to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name             "jungledisk"
 maintainer       "Mudbug Media, Inc."
 maintainer_email "info@mudbugmedia.com"
 license          "Apache 2.0"
 description      "Installs/Configures jungledisk"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.2"
+version          "0.0.3"
 supports 'ubuntu'
 recipe "jungledisk::default", "Download and install JungleDiskServer package, and configure the license xml"
 attribute 'jungledisk/serial',
